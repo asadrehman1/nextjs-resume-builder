85488b4ac71b5f5f570b0b1f8da769367c9c289e
asadrehman1 <asadrehman7890661@gmail.com>
2025-03-05 02:05:11 +0500

    Added educations form, its schema validation and prisma schema

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -2,6 +2,7 @@ import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
+import EducationForm from "./forms/EducationForm";
 
 export const steps: {
   title: string;
@@ -10,5 +11,14 @@ export const steps: {
 }[] = [
   { title: "General Info", component: GeneralInfoForm, key: "general-info" },
   { title: "Personal Info", component: PersonalInfoForm, key: "personal-info" },
-  { title: "Work Experience", component: WorkExperienceForm, key: "work-experience" },
+  {
+    title: "Work Experience",
+    component: WorkExperienceForm,
+    key: "work-experience",
+  },
+  {
+    title: "Education",
+    component: EducationForm,
+    key: "education",
+  },
 ];
